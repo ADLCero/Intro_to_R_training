577a6bc65ec4043ffafb29ff34e9ea2bb43193c0
ADLCero <amcero@syr.edu>
2025-10-20 15:09:03 +0800

    Changes in file organization

--- 01_Scripts/Packages_for_Rmd.R
+++ 01_Scripts/Packages_for_Rmd.R
@@ -1,6 +1,9 @@
 
+# Code for installing R Markdown and supporting packages
+
 # Install the following packages:
 
 install.packages("rmarkdown")
 install.packages("knitr")
 install.packages("tinytex") # for PDF output
+
